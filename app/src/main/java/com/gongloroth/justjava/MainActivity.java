e1460515723d73d832d18be6fa8f491250b0ca22
gongloroth <gnambence@gmail.com>
2017-12-23 18:08:32 +0100

    7th commit
    Finished JustJava app with different solutions at some point compared to the one in the course

--- app/src/main/java/com/gongloroth/justjava/MainActivity.java
+++ app/src/main/java/com/gongloroth/justjava/MainActivity.java
@@ -1,11 +1,16 @@
 package com.gongloroth.justjava;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.Formatter;
@@ -18,10 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private final int priceOfOneCup = 150;
     private final int WHIPPED_CREAM_PRICE = 30;
     private final int CHOCOLATE_PRICE = 25;
-    private int quantity = 0;
+    private int quantity = 1;
     private int price = 0;
     private String message = "Thank you!";
-    private String name = "gongloroth";
+    private String name = "";
     private boolean hasWhippedCream = false;
     private boolean hasChocolate = false;
     private CheckBox whippedCreamCheckBox;
@@ -59,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt("PRICE", price);
         outState.putBoolean("WHIPPED_KEY", hasWhippedCream);
         outState.putBoolean("CHOCOLATE_KEY", hasChocolate);
+        outState.putString("PERSON_NAME", name);
 
         // call superclass to save any view hierarchy
         super.onSaveInstanceState(outState);
@@ -70,14 +76,14 @@ public class MainActivity extends AppCompatActivity {
         quantity = savedInstanceState.getInt("QUANTITY");
         hasWhippedCream = savedInstanceState.getBoolean("WHIPPED_KEY");
         hasChocolate = savedInstanceState.getBoolean("CHOCOLATE_KEY");
+        name = savedInstanceState.getString("PERSON_NAME");
 
         getWhippedCreamCheckBox().setChecked(hasWhippedCream);
         getChocolateCheckBox().setChecked(hasChocolate);
 
         displayQuantity(quantity);
-        if (price > 0) {
-            displayMessage(createOrderSummary(price, hasWhippedCream, hasChocolate));
-        }
+        //displayMessage(createOrderSummary(name, price, hasWhippedCream, hasChocolate));
+
     }
 
     /**
@@ -88,16 +94,21 @@ public class MainActivity extends AppCompatActivity {
         //int price = calculatePrice(quantity,priceOfOneCup);
         //displayPrice(price);
 
-        if (hasWhippedCream && hasChocolate) {
-            price = calculatePrice(quantity, priceOfOneCup) + WHIPPED_CREAM_PRICE * quantity + CHOCOLATE_PRICE * quantity;
-        } else if (hasWhippedCream && !hasChocolate) {
-            price = calculatePrice(quantity, priceOfOneCup) + WHIPPED_CREAM_PRICE * quantity;
-        } else if (hasChocolate && !hasWhippedCream) {
-            price = calculatePrice(quantity, priceOfOneCup) + CHOCOLATE_PRICE * quantity;
-        } else {
-            price = calculatePrice(quantity, priceOfOneCup);
-        }
-        displayMessage(createOrderSummary(price, hasWhippedCream, hasChocolate));
+        EditText personNameText = (EditText) findViewById(R.id.person_name);
+        name = personNameText.getText().toString();
+        Log.v("MainActivity", "Person's name: " + name);
+
+        price = calculatePrice(quantity, priceOfOneCup);
+
+        //displayMessage(createOrderSummary(name, price, hasWhippedCream, hasChocolate));
+        String subject = "JustJava order summary for " + name;
+        composeEmail(subject, createOrderSummary(name,price,hasWhippedCream,hasChocolate));
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse("geo:47.6,-122.3"));
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
+
     }
 
     /**
@@ -130,34 +141,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
+        if (quantity == 100) {
+            Context context = getApplicationContext();
+            CharSequence text = "You cannot have more than 99 coffees";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            return;
+        }
+
         quantity++;
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
+
+        if (quantity == 1) {
+            Context context = getApplicationContext();
+            CharSequence text = "You cannot have less than 1 coffee";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            return;
+        }
+
         quantity--;
         displayQuantity(quantity);
     }
 
     private int calculatePrice(int quantity, int priceOfOneCup) {
-        return quantity * priceOfOneCup;
+
+        int price_temp = priceOfOneCup;
+
+        if (hasWhippedCream) {
+            price_temp += WHIPPED_CREAM_PRICE;
+        }
+
+        if (hasChocolate) {
+            price_temp += CHOCOLATE_PRICE;
+        }
+
+        return price_temp * quantity;
     }
 
     private String formatCurrency(int number) {
         return NumberFormat.getCurrencyInstance().format(number);
     }
 
-    private String createOrderSummary(int price, boolean creamIsChecked, boolean chocolateIsChecked) {
+    private String createOrderSummary(String name, int price, boolean creamIsChecked, boolean chocolateIsChecked) {
         StringBuilder sb = new StringBuilder();
         Formatter fmt = new Formatter(sb);
+
         fmt.format("Name: %1$s\nQuantity: %2$d\nTotal: %3$s\n%4$s", name, quantity, formatCurrency(price), message);
         Log.v("MainActivity", "Add whipped cream? " + creamIsChecked);
         Log.v("MainActivity", "Add chocolate? " + chocolateIsChecked);
         if (creamIsChecked) {
-            sb.insert(sb.indexOf("\n") + 1, "Whipped cream added for " + formatCurrency(WHIPPED_CREAM_PRICE * quantity) + ".\n");
+            sb.insert(sb.indexOf("\n") + 1, getString(R.string.order_summary_whipped_cream, formatCurrency(WHIPPED_CREAM_PRICE * quantity)) + ".\n");
         }
         if (chocolateIsChecked) {
-            sb.insert(sb.indexOf("\n") + 1, "Chocolate added for " + formatCurrency(CHOCOLATE_PRICE * quantity) + ".\n");
+            sb.insert(sb.indexOf("\n") + 1, getString(R.string.order_summary_chocolate, formatCurrency(CHOCOLATE_PRICE * quantity)) + ".\n");
         }
 
         return sb.toString();
@@ -203,5 +249,16 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    public void composeEmail(String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 
 }
